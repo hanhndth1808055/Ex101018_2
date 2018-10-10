7fe63ca8b4af5e3be83578cc0f5e9d52f26e35d4
HanhND <hanhndth1808055@fpt.edu.vn>
2018-10-10 20:25:28 +0700

    Luong thang 13

--- main.c
+++ main.c
@@ -1,49 +1,53 @@
 #include <stdio.h>
 
-float luongt13(int *namKinhNghiem, int *namLamViec) {
+int luongt13(int *namKinhNghiem, int *namLamViec) {
     int luongChinhThuc;
-    if (namKinhNghiem <= 2) {
+    if (*namKinhNghiem <= 2) {
         luongChinhThuc = 10000000;
     }
-    if (namKinhNghiem > 2 && namKinhNghiem <= 5) {
+    if (*namKinhNghiem > 2 && *namKinhNghiem <= 5) {
         luongChinhThuc = 20000000;
     }
-    if (namKinhNghiem > 5) {
+    if (*namKinhNghiem > 5) {
         luongChinhThuc = 20000000;
     }
-if(namLamViec <= 1){
-    printf("Luong thang 13 la: %.2f", luongChinhThuc*0.3);
-}
-if(namLamViec > 1 && namLamViec <= 2){
-    printf("Luong thang 13 la: %.2f", luongChinhThuc*0.5);
-}
-    if(namLamViec > 2 && namLamViec <= 5){
-        printf("Luong thang 13 la: %.2f", luongChinhThuc);
+    if (*namLamViec <= 1) {
+        printf("Luong thang 13 la: %.2f\n", (float) luongChinhThuc * 0.3);
     }
-    if(namLamViec > 5){
-        printf("Luong thang 13 la: %.2f", luongChinhThuc*2);
+    if (*namLamViec > 1 && *namLamViec <= 2) {
+        printf("Luong thang 13 la: %.2f\n", (float) luongChinhThuc * 0.5);
     }
+    if (*namLamViec > 2 && *namLamViec <= 5) {
+        printf("Luong thang 13 la: %.2f\n", (float) luongChinhThuc);
+    }
+    if (*namLamViec > 5) {
+        printf("Luong thang 13 la: %.2f\n", (float) luongChinhThuc * 2);
+    }
+    return 0;
 }
 
 int main() {
-    int namKinhNghiem, namLamViec;
+    int namKinhNghiem;
+    int namLamViec;
     printf("Nhap so nam kinh nghiem.\n");
-    scanf("%d", namKinhNghiem);
+    scanf("%d", &namKinhNghiem);
     printf("Nhap so nam lam viec.\n");
-    scanf("%d", namLamViec);
-    luongt13(&namLamViec, &namKinhNghiem);
+    scanf("%d", &namLamViec);
+    luongt13(&namKinhNghiem, &namLamViec);
 
-char choice;
-    while(1==1){
+    char choice;
+
+    while (1) {
         printf("Nhap n/N neu muon dung chuong trinh.\n");
-     if(choice = 'n' || choice = 'N'){
-         break;
-    }else{
-         luongt13(&namLamViec, &namKinhNghiem);
-     }
-}
+        scanf("%c", &choice);
+        if (choice == 'n' || choice == 'N') {
+            printf("Dung chuong trinh.");
+            break;
+        } else {
+            luongt13(&namKinhNghiem, &namLamViec);
+        }
+    }
 
-    luongt13(&namLamViec, &namKinhNghiem);
 
     return 0;
 }
